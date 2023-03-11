c1d488f5ddb230df82fea23a52031b5c99cc60e1
leegunsun <talkcjstk@naver.com>
2023-03-11 20:14:31 +0900

    good

--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -1,4 +1,5 @@
 const SignupService = require("../services/signup.service");
+const Boom = require("boom");
 
 class SignupController {
   constructor() {
@@ -8,57 +9,25 @@ class SignupController {
   createSignup = async (req, res, next) => {
     const { nickname, password, confirm } = req.body;
 
-    const nicknameRegex = /^[A-Za-z0-9]{3,}$/;
-    const passwordLengthRegex = /^[A-Za-z0-9]{4,}$/;
-    const passwordRegex = new RegExp(`^(?!.*${nickname}).+$`);
-
     try {
-      if (nicknameRegex.test(nickname)) {
-        const existsUsers = await this.signupService.findOneUser(nickname);
-
-        // 이미 존재하는 닉네임
-        if (existsUsers) {
-          return res.status(412).json({
-            errorMessage: "중복된 닉네임입니다.",
-          });
-        }
-
-        //패스워드에 닉네임이 포함됨
-        if (passwordLengthRegex.test(password)) {
-          if (!passwordRegex.test(password)) {
-            return res
-              .status(412)
-              .json({ errorMessage: "패스워드에 닉네임이 포함되어 있습니다." });
-          }
-
-          // 패스워드 확인이 실패함
-          if (password !== confirm) {
-            return res
-              .status(412)
-              .json({ errorMessage: "패스워드가 일치하지 않습니다." });
-          }
-          const createSignupData = await this.signupService.createSignup(
-            nickname,
-            password
-          );
-          res.status(201).json({ message: "회원 가입에 성공하였습니다." });
-          return createSignupData;
-        } else {
-          //패스워드 요구 조건이 맞지않음
-          return res
-            .status(412)
-            .json({ errorMessage: "패스워드 형식이 일치하지 않습니다." });
-        }
-      } else {
-        // 닉네임이 요구 조건에 맞지 않음
-        return res
-          .status(412)
-          .json({ errorMessage: "닉네임의 형식이 일치하지 않습니다." });
-      }
+      const createSignupData = await this.signupService.createSignup(
+        nickname,
+        password,
+        confirm
+      );
+      res.status(201).json({ message: "회원 가입에 성공하였습니다." });
+      return createSignupData;
     } catch (error) {
-      return res
-        .status(400)
-        .json({ errorMessage: "요청한 데이터 형식이 올바르지 않습니다." });
+      if (Boom.isBoom(error)) {
+        const errorOutput = error.output;
+        // Boom 오류 객체인 경우, 에러 메시지와 함께 HTTP 응답을 보냅니다.
+        res
+          .status(errorOutput.statusCode)
+          .json({ errorMessage: errorOutput.payload.message });
+      } else {
+        // 그 외의 일반적인 에러인 경우, 500 상태 코드와 함께 에러 메시지를 보냅니다.
+        res.status(500).json({ message: error.message });
+      }
     }
   };
 }
--- services/signup.service.js
+++ services/signup.service.js
@@ -1,26 +1,58 @@
 const SignupRepository = require("../repositories/signup.repository");
+const Boom = require("boom");
 
 class SignupService {
   constructor() {
     this.signupRepository = new SignupRepository();
   }
 
-  createSignup = async (nickname, password) => {
-    const maxUserId = await this.signupRepository.findSortUserId();
-    const userId = maxUserId ? maxUserId.userId + 1 : 1;
-    const createSignupData = await this.signupRepository.createSignup(
-      nickname,
-      password,
-      userId
-    );
-
-    return createSignupData;
-  };
-
   findOneUser = async (nickname) => {
     const findUser = await this.signupRepository.findOneUser(nickname);
 
     return findUser;
+  };
+
+  createSignup = async (nickname, password, confirm) => {
+    const nicknameRegex = /^[A-Za-z0-9]{3,}$/;
+    const passwordLengthRegex = /^[A-Za-z0-9]{4,}$/;
+    const passwordRegex = new RegExp(`^(?!.*${nickname}).+$`);
+    const maxUserId = await this.signupRepository.findSortUserId();
+    const userId = maxUserId ? maxUserId.userId + 1 : 1;
+
+    if (nicknameRegex.test(nickname)) {
+      const existsUsers = await this.signupRepository.findOneUser(nickname);
+
+      // 이미 존재하는 닉네임
+      if (existsUsers) {
+        throw Boom.preconditionFailed("중복된 닉네임입니다.");
+      }
+
+      //패스워드에 닉네임이 포함됨
+      if (passwordLengthRegex.test(password)) {
+        if (!passwordRegex.test(password)) {
+          throw Boom.preconditionFailed(
+            "패스워드에 닉네임이 포함되어 있습니다."
+          );
+        }
+
+        // 패스워드 확인이 실패함
+        if (password !== confirm) {
+          throw Boom.preconditionFailed("패스워드가 일치하지 않습니다.");
+        }
+        const createSignupData = await this.signupRepository.createSignup(
+          nickname,
+          password,
+          userId
+        );
+        return createSignupData;
+      } else {
+        //패스워드 요구 조건이 맞지않음
+        throw Boom.preconditionFailed("패스워드 형식이 일치하지 않습니다.");
+      }
+    } else {
+      // 닉네임이 요구 조건에 맞지 않음
+      throw Boom.preconditionFailed("닉네임의 형식이 일치하지 않습니다.");
+    }
   };
 }
 
