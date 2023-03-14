750d2fc01b71e2caa4e6b9f0fda3aa6f6c59bc6d
leegunsun <talkcjstk@naver.com>
2023-03-14 11:37:11 +0900

    test

--- slack.bot.js
+++ slack.bot.js
@@ -1,126 +1,45 @@
-const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const app = express();
-const port = 3002;
-const SECRET_KEY = `HangHae99`;
+class A {
+  constructor(log) {
+    console.log("log", log);
+    this.log = log;
 
-console.log(test);
+    this.b = new B();
+  }
 
-// app.use(cookieParser());
-// let tokenObject = {}; // Refresh Token을 저장할 Object
+  methodA() {
+    console.log("methodA", this.log);
+  }
+}
 
-// app.get("/set-token/:id", (req, res) => {
-//   const id = req.params.id;
-//   const accessToken = createAccessToken(id);
-//   const refreshToken = createRefreshToken();
+class B {
+  constructor() {}
 
-//   tokenObject[refreshToken] = id; // Refresh Token을 가지고 해당 유저의 정보를 서버에 저장합니다.
-//   res.cookie("accessToken", accessToken); // Access Token을 Cookie에 전달한다.
-//   res.cookie("refreshToken", refreshToken); // Refresh Token을 Cookie에 전달한다.
+  static sta(a) {
+    console.log(a);
+  }
 
-//   return res
-//     .status(200)
-//     .send({ message: "Token이 정상적으로 발급되었습니다." });
-// });
+  methodB() {
+    console.log("Method B");
+  }
+}
 
-// // Access Token을 생성합니다.
-// function createAccessToken(id) {
-//   const accessToken = jwt.sign(
-//     { id: id }, // JWT 데이터
-//     SECRET_KEY, // 비밀키
-//     { expiresIn: "10s" }
-//   ); // Access Token이 10초 뒤에 만료되도록 설정합니다.
+const a = new A();
+a.b.methodB();
 
-//   return accessToken;
-// }
-
-// // Refresh Token을 생성합니다.
-// function createRefreshToken() {
-//   const refreshToken = jwt.sign(
-//     {}, // JWT 데이터
-//     SECRET_KEY, // 비밀키
-//     { expiresIn: "7d" }
-//   ); // Refresh Token이 7일 뒤에 만료되도록 설정합니다.
-
-//   return refreshToken;
-// }
-
-// app.get("/get-token", (req, res) => {
-//   const accessToken = req.cookies.accessToken;
-//   const refreshToken = req.cookies.refreshToken;
-
-//   if (!refreshToken)
-//     return res
-//       .status(400)
-//       .json({ message: "Refresh Token이 존재하지 않습니다." });
-//   if (!accessToken)
-//     return res
-//       .status(400)
-//       .json({ message: "Access Token이 존재하지 않습니다." });
-
-//   const isAccessTokenValidate = validateAccessToken(accessToken);
-//   const isRefreshTokenValidate = validateRefreshToken(refreshToken);
-
-//   if (!isRefreshTokenValidate)
-//     return res.status(419).json({ message: "Refresh Token이 만료되었습니다." });
-
-//   if (!isAccessTokenValidate) {
-//     const accessTokenId = tokenObject[refreshToken];
-//     if (!accessTokenId)
-//       return res
-//         .status(419)
-//         .json({ message: "Refresh Token의 정보가 서버에 존재하지 않습니다." });
-
-//     const newAccessToken = createAccessToken(accessTokenId);
-//     res.cookie("accessToken", newAccessToken);
-//     return res.json({ message: "Access Token을 새롭게 발급하였습니다." });
-//   }
-
-//   const { id } = getAccessTokenPayload(accessToken);
-//   return res.json({
-//     message: `${id}의 Payload를 가진 Token이 성공적으로 인증되었습니다.`,
-//   });
-// });
-
-// // Access Token을 검증합니다.
-// function validateAccessToken(accessToken) {
-//   try {
-//     jwt.verify(accessToken, SECRET_KEY); // JWT를 검증합니다.
-//     return true;
-//   } catch (error) {
-//     return false;
+// // class의 extends와 this.a = new A()의 차이는 뭘까?
+// // 실험 1 : this.a = new A("이름", 32) 를 입력하면 어떻게 될까?
+// // 실험 2 : constructor(name, age) {
+// //     this.a = new A(name, age);
+// //   }
+// class C {
+//   constructor() {
+//     this.a = new A();
 //   }
 // }
 
-// // Refresh Token을 검증합니다.
-// function validateRefreshToken(refreshToken) {
-//   try {
-//     jwt.verify(refreshToken, SECRET_KEY); // JWT를 검증합니다.
-//     return true;
-//   } catch (error) {
-//     return false;
-//   }
-// }
-
-// // Access Token의 Payload를 가져옵니다.
-// function getAccessTokenPayload(accessToken) {
-//   try {
-//     const payload = jwt.verify(accessToken, SECRET_KEY); // JWT에서 Payload를 가져옵니다.
-//     return payload;
-//   } catch (error) {
-//     return null;
-//   }
-// }
-// app.get("/", (req, res) => {
-//   res.status(200).send("Hello Token!");
-// });
-
-// app.listen(port, () => {
-//   console.log(port, "포트로 서버가 열렸어요!");
-// });
-
-/////
+// const c = new C();
+// c.a.methodA(); // 출력 결과: Method A
+// c.b.methodB(); // 출력 결과: Method B
 
 // const Slack = require("slack-node");
 
